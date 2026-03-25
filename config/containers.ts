3dbc4e618b722f287f655012a1b2c117de04063b
root <root@DESKTOP-I2NH4E6.localdomain>
2026-03-25 19:41:41 +0300

    session2

--- config/containers.ts
+++ config/containers.ts
@@ -1,11 +1,34 @@
 import { PrismaUserRepository } from "@/adapters/repositories";
 import { prisma } from "@/adapters/repositories";
 import { RegisterUserUseCase } from "@/core/usecases/register-user";
+import type { PrismaClient } from "@prisma/client";
+import type { UserRepository } from "@/core/ports/repositories";
 
-const userRepository = new PrismaUserRepository(prisma);
+interface Container {
+  prisma: PrismaClient;
+  userRepository: UserRepository;
+  registerUser: RegisterUserUseCase;
+}
 
-export const container = {
-  prisma,
-  userRepository,
-  registerUser: new RegisterUserUseCase(userRepository),
-};
+function createContainer(): Container {
+  const userRepository = new PrismaUserRepository(prisma);
+  return {
+    prisma,
+    userRepository,
+    registerUser: new RegisterUserUseCase(userRepository),
+  };
+}
+
+export const container: Container = createContainer();
+
+export function getPrisma(): PrismaClient {
+  return container.prisma;
+}
+
+export function getUserRepository(): UserRepository {
+  return container.userRepository;
+}
+
+export function getRegisterUserUseCase(): RegisterUserUseCase {
+  return container.registerUser;
+}
